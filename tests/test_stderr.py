26b3b1c9d9ce49a57df96afe420fe37266679ef8
Gregory Hellings <greg.hellings@gmail.com>
2020-02-12 11:41:28 -0600

    Fetch libc everywhere

--- tests/test_stderr.py
+++ tests/test_stderr.py
@@ -1,13 +1,17 @@
 from __future__ import print_function
-import ctypes
 import os
 import six
 import sys
 from unittest import TestCase
 from stream_redirect import Redirect
+from stream_redirect.complete_redirector import CompleteRedirector
 
 
 class TestStderr(TestCase):
+    @classmethod
+    def setUpClass(self):
+        self.libc = CompleteRedirector("stdout").libc
+
     def test_gets_python_output(self):
         """
         Tests that only ouptut from the Python code will be collected and not
@@ -17,15 +21,13 @@ class TestStderr(TestCase):
         stdout_not_found = "stdout string"
         not_found = "not found"
         also = "also not found"
-        # Write directly out from C code
-        libc = ctypes.CDLL(None)
         # Do the attempt
         cm = Redirect(python_only=True, stderr=True, stdout=False)
         with cm:
             print(expected_output, file=sys.stderr)
             print(stdout_not_found)
             os.system("echo {not_found}")
-            libc.puts(six.ensure_binary(also))
+            self.libc.puts(six.ensure_binary(also))
         self.assertIn(expected_output, cm.stderr)
         self.assertNotIn(stdout_not_found, cm.stderr)
         self.assertNotIn(not_found, cm.stderr)
@@ -39,13 +41,12 @@ class TestStderr(TestCase):
         py_out = "python string"
         sys_call = "system call"
         c_code = "c code"
-        libc = ctypes.CDLL(None)
         # Make the attempt
         cm = Redirect(stderr=True, stdout=False)
         with cm:
             print(py_out, file=sys.stderr)
             os.system("echo {} 1>&2".format(sys_call))
-            libc.puts(six.ensure_binary(c_code))
+            self.libc.puts(six.ensure_binary(c_code))
         self.assertIn(py_out, cm.stderr)
         self.assertIn(sys_call, cm.stderr)
         self.assertNotIn(c_code, cm.stderr)  # puts still doesn't go to stderr
--- tests/test_stdout.py
+++ tests/test_stdout.py
@@ -1,13 +1,17 @@
 from __future__ import print_function
-import ctypes
 import os
 import six
 import sys
 from unittest import TestCase
 from stream_redirect import Redirect
+from stream_redirect.complete_redirector import CompleteRedirector
 
 
 class TestStdout(TestCase):
+    @classmethod
+    def setUpClass(self):
+        self.libc = CompleteRedirector("stdout").libc
+
     def test_gets_python_output(self):
         """
         Tests that only ouptut from the Python code will be collected and not
@@ -17,15 +21,13 @@ class TestStdout(TestCase):
         stderr_not_found = "stderr string"
         not_found = "not found"
         also = "also not found"
-        # Write directly out from C code
-        libc = ctypes.CDLL(None)
         # Do the attempt
         cm = Redirect(python_only=True)
         with cm:
             print(expected_output)
             print(stderr_not_found, file=sys.stderr)
             os.system("echo {not_found}")
-            libc.puts(six.ensure_binary(also))
+            self.libc.puts(six.ensure_binary(also))
         self.assertIn(expected_output, cm.stdout)
         self.assertNotIn(stderr_not_found, cm.stdout)
         self.assertNotIn(not_found, cm.stdout)
@@ -39,13 +41,12 @@ class TestStdout(TestCase):
         py_out = "python string"
         sys_call = "system call"
         c_code = "c code"
-        libc = ctypes.CDLL(None)
         # Make the attempt
         cm = Redirect()
         with cm:
             print(py_out)
             os.system("echo {}".format(sys_call))
-            libc.puts(six.ensure_binary(c_code))
+            self.libc.puts(six.ensure_binary(c_code))
         self.assertIn(py_out, cm.stdout)
         self.assertIn(sys_call, cm.stdout)
         self.assertIn(c_code, cm.stdout)
