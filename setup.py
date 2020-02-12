26dd0ec93217d30249da22a07f8e31a05aef0957
Gregory Hellings <greg.hellings@gmail.com>
2020-02-11 20:58:04 -0600

    Runs on 3.4, so let it run on 3.4

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     package_dir={'': 'src'},
     packages=find_packages('src'),
     install_requires=[],
-    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
